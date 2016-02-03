dc70662fdbc12862cb36711ff6bbc1cbb35cc2ae
H4jen <alterweggo2@gmail.com>
2016-02-03 22:08:52 +0100

    QAM engine entry point

---
+++ QAM_engine_entry.m
@@ -0,0 +1,11 @@
+% QAM engine
+% Starting point of engine
+% 2016-02-03 
+
+
+%Use box to stop look execution.
+%stoploop();
+FS = stoploop({'To stop engine press OK button'}) ;
+while(~FS.Stop())
+  % insert code here
+end
